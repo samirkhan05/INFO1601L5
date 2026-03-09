e65e7360b21a3774f977a56b1e56227ad5ce52d8
samirkhan05 <samirkhann8600@gmail.com>
2026-03-08 23:30:27 -0400

    task 6.1

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-//prints current date
-function printDate(){
-   console.log(Date().toLocaleTimeString());
-}
-      
-setInterval(printDate, 1000);
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));//true
+
+arr.push(11);//adds item to the end
+
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
+
+let lastItem == arr.pop();//removes last item
+
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
