5ff647dd51e27ffabd98cd9af8b1e2b89c1e4553
samirkhan05 <samirkhann8600@gmail.com>
2026-03-08 23:26:41 -0400

    task 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
+let radius = 7;
+const pi = 3.14;
+
+let area = radius * radius * pi;
