39dae0a0f23ef3b83298f0823145c0a4e8a72ce1
samirkhan05 <samirkhann8600@gmail.com>
2026-03-08 23:27:38 -0400

    task 3

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+let name = "bob";
+let age = 24;
 
-let radius = 7;
-const pi = 3.14;
+console.log(typeof(name))//string
+console.log(typeof(age))//number
 
-let area = radius * radius * pi;
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
