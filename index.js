c7c49c4335ae0d8f8bbe6d6c5b2042fc895a17b6
samirkhan05 <samirkhann8600@gmail.com>
2026-03-08 23:29:46 -0400

    task 6

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-function happyPrint(string){
- console.log("😀: "+string);
+//prints current date
+function printDate(){
+   console.log(Date().toLocaleTimeString());
 }
-
-function sadPrint(string){
- console.log("😢: "+string);
-}
-
-//This high-order function adds 2 parameters and passes the answer to the callback
-function add(a, b, callback){
-   let ans = a + b;
-   callback(ans);// call the callback and pass the answer to it
-}
-
-//call add passing the callbacks to it
-
-add( 5,10,happyPrint)
-add(11, 12, sadPrint)
+      
+setInterval(printDate, 1000);
