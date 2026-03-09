ed4c3a07e5dd1ce3a41772ebf41b2b34e3002dd6
samirkhan05 <samirkhann8600@gmail.com>
2026-03-08 23:32:58 -0400

    task 7

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-let arr = [12, 33, 4, 5, -4, 8, 19, 25];
-                
-//map() creates a new array from the elements of one without changing the old one
-function double(num){
-  return num * 2;
+//Create a constructor a functions which builds object for us
+function createPerson(name, height, weight) {
+  return { name: name, height: height, weight: weight };
 }
 
-let doubledArr = arr.map(double); 
-console.log(doubledArr);
-
-function isOdd(num){
-  return element%2 !== 0; 
-}
-//Filter takes a test condition and returns only the element which 
-//make the condition true
-let odds = arr.filter(isOdd);
-console.log(odds);
-
-//Returns true or false if any of the elements of the array 
-//meets a specified condition
-function has5Factor(){
-  return ele % 5 === 0;
+function calcBMI(weight, height) {
+  return weight / (height * height);
 }
 
-let hasFiveFactor = arr.some(has5Factor);
-console.log(hasFiveFactor);
-
-function intCompare(a, b){
- return a - b;
+function avgBMI(people) {
+  let sum = 0;
+  for (let person of people) {
+    //sum the bmi of each person
+    sum += calcBMI(person.weight, person.height);
+  }
+  //calculate average
+  return sum / people.length;
 }
 
-//sort function must return either 0, +ve, -ve
-let ascending = arr.sort(intCompare)
-console.log(ascending);
+//create a collection of people
+let people = [
+  createPerson("Sally", 60, 2.5),
+  createPerson("Ben", 81, 3),
+  createPerson("Shelly", 50, 1.7)
+];
 
-
+console.log(avgBMI(people));
